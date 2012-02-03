3c6920ba1b2976574405cf90a5cb48473b86af70
unknown <foijord@gmail.com>
2012-02-03 16:02:34 +0100

    shell script for DAG generation

---
+++ generate_dag.sh
@@ -0,0 +1,7 @@
+./combinatorics.py -n A K Q J T 9 8 7 6 5 4 3 2 -r 1 >  dag.dat
+./combinatorics.py -n A K Q J T 9 8 7 6 5 4 3 2 -r 2 >> dag.dat
+./combinatorics.py -n A K Q J T 9 8 7 6 5 4 3 2 -r 3 >> dag.dat
+./combinatorics.py -n A K Q J T 9 8 7 6 5 4 3 2 -r 4 >> dag.dat
+./combinatorics.py -n A K Q J T 9 8 7 6 5 4 3 2 -r 5 >> dag.dat
+./combinatorics.py -n A K Q J T 9 8 7 6 5 4 3 2 -r 6 >> dag.dat
+./combinatorics.py -n A K Q J T 9 8 7 6 5 4 3 2 -r 7 >> dag.dat
