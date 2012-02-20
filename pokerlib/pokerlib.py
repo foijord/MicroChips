43c3f34f00e46ebb78767d9c64710b60a12dfd5c
Frode Oijord <foijord@gmail.com>
2012-02-20 21:46:01 +0100

    moved rank generation to separate script

--- pokerlib/pokerlib.py
+++ pokerlib/pokerlib.py
@@ -1,28 +1,14 @@
 #!/usr/bin/env python
 
-import sys
-import os
-
 from itertools import *
 from constants import *
 
 class FiveEval:
     def __init__(self) :
-                
-        self.ranks = [0] * (max_5_card_rank_key + 1)
-        self.flushes = [0] * (max_5_card_flush_key + 1)
-                
-        equivalence_classes = open("equivalence_classes.dat", 'r')
-        for line in equivalence_classes:
-            columns = line.split('\t')
+        self.ranks = [int(rank) for rank in open("ranks_5.dat", "r")]
+        self.flushes = [int(flush) for flush in open("flushes_5.dat", "r")]
 
-            if "Flush" in columns[2]:
-                key = sum([flush_keys[c] for c in [card_ranks.index(c) for c in columns[0]]])
-                self.flushes[key] = int(columns[1])
-            else:
-                key = sum([rank_keys5[c] for c in [card_ranks.index(c) for c in columns[0]]])
-                self.ranks[key] = int(columns[1])
-                                
+                
     def getRankOfFive(self, c1, c2, c3, c4, c5) :
         if (c1 & 3 == c2 & 3 == c3 & 3 == c4 & 3 == c5 & 3):
             return self.flushes[flush_keys[c1 >> 2] +
@@ -50,34 +36,10 @@ class FiveEval:
 
 class SevenEval:
     def __init__(self):
-        self.ranks = [0] * (max_7_card_rank_key + 1)
-        self.flushes = [0] * (max_7_card_flush_key + 1)
-        self.flushCheck = [-1] * (7 * max(suit_keys7) + 1)
+        self.ranks = [int(rank) for rank in open("ranks_7.dat", "r")]
+        self.flushes = [int(flush) for flush in open("flushes_7.dat", "r")]
+        self.flushcheck = [int(flushcheck) for flushcheck in open("flushcheck_7.dat", "r")]
         self.deck_keys = [(rank_keys7[n >> 2] << flush_bit_shift) + suit_keys7[n & 3] for n in range(52)]
-        
-        five_eval = FiveEval()
-        spades = [i * 4 for i in range(13)]
-
-        # All combinations, except flushes
-        for c in combinations_with_replacement(spades, 7):
-            card_count = [c.count(i) for i in c]
-            if (max(card_count) < 5):
-                key = sum([rank_keys7[i >> 2] for i in c])
-                self.ranks[key] = five_eval.getRank([c[0], c[1], c[2], c[3], c[4] + 1, c[5] + 1, c[6] + 1])
-
-        # All flushes
-        for n in [5, 6, 7]:
-            for c in combinations(spades, n):
-                key = sum([flush_keys[i >> 2] for i in c])
-                self.flushes[key] = five_eval.getRank(c)
-
-        # Flush checks
-        for c in combinations_with_replacement(suit_keys7, 7):
-            suit_count = [c.count(i) for i in suit_keys7]
-            max_count = max(suit_count)
-            if max_count >= 5:
-                suit_index = suit_count.index(max_count) 
-                self.flushCheck[sum(c)] = suit_keys7[suit_index]
 
     def getRankOfSeven(self, c1, c2, c3, c4, c5, c6, c7):
         # Create a 7-card hand key by adding up each of the card keys.
@@ -90,7 +52,7 @@ class SevenEval:
             self.deck_keys[c7]
 
         # Tear off the flush check strip.
-        flush_suit = self.flushCheck[key & flush_bit_mask]
+        flush_suit = self.flushcheck[key & flush_bit_mask]
                 
         if flush_suit < 0:
             # Tear off the non-flush key strip, and look up the rank.
---
+++ pokerlib/rankgen.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+from itertools import *
+from constants import *
+from pokerlib import *
+
+def generate_5_card_ranks():
+    ranks = [0] * (max_5_card_rank_key + 1)
+    flushes = [0] * (max_5_card_flush_key + 1)
+
+    equivalence_classes = open("equivalence_classes.dat", 'r')
+    for line in equivalence_classes:
+        columns = line.split('\t')
+        
+        if "Flush" in columns[2]:
+            key = sum([flush_keys[c] for c in [card_ranks.index(c) for c in columns[0]]])
+            flushes[key] = int(columns[1])
+        else:
+            key = sum([rank_keys5[c] for c in [card_ranks.index(c) for c in columns[0]]])
+            ranks[key] = int(columns[1])
+
+    ranks_5 = open("ranks_5.dat", "w+")
+    for i in ranks:
+        ranks_5.write("%i\n" % i)
+
+    flushes_5 = open("flushes_5.dat", "w+")
+    for i in flushes:
+        flushes_5.write("%i\n" % i)
+
+def generate_7_card_ranks():
+        ranks = [0] * (max_7_card_rank_key + 1)
+        flushes = [0] * (max_7_card_flush_key + 1)
+        flushcheck = [-1] * (7 * max(suit_keys7) + 1)
+        
+        five_eval = FiveEval()
+        spades = [i * 4 for i in range(13)]
+
+        # All combinations, except flushes
+        for c in combinations_with_replacement(spades, 7):
+            card_count = [c.count(i) for i in c]
+            if (max(card_count) < 5):
+                key = sum([rank_keys7[i >> 2] for i in c])
+                ranks[key] = five_eval.getRank([c[0], c[1], c[2], c[3], c[4] + 1, c[5] + 1, c[6] + 1])
+
+        # All flushes
+        for n in [5, 6, 7]:
+            for c in combinations(spades, n):
+                key = sum([flush_keys[i >> 2] for i in c])
+                flushes[key] = five_eval.getRank(c)
+
+        # Flush checks
+        for c in combinations_with_replacement(suit_keys7, 7):
+            suit_count = [c.count(i) for i in suit_keys7]
+            max_count = max(suit_count)
+            if max_count >= 5:
+                suit_index = suit_count.index(max_count) 
+                flushcheck[sum(c)] = suit_keys7[suit_index]
+
+        ranks_7 = open("ranks_7.dat", "w+")
+        for i in ranks:
+            ranks_7.write("%i\n" % i)
+
+        flushes_7 = open("flushes_7.dat", "w+")
+        for i in flushes:
+            flushes_7.write("%i\n" % i)
+
+        flushcheck_7 = open("flushcheck_7.dat", "w+")
+        for i in flushcheck:
+            flushcheck_7.write("%i\n" % i)
+
+
+
+if __name__ == "__main__":
+    generate_5_card_ranks()
+    generate_7_card_ranks()
